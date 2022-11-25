95cba2a1caf462530bf46fd85e9e65b94268be15
Vladislav Fedorov <vldslv.valeryevich@gmail.com>
2022-11-25 11:44:05 -0600

    Dinosaurs - Idle, Run, Death animations and functionality

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,12 +7,37 @@ public class CharacterMovement : MonoBehaviour
     public float speed = 10f;
     public float maxSpeed = 20f;
     public Rigidbody2D rb;
+    public Animator anim;
+    public SpriteRenderer rend;
+
+    bool moving;
+
+    bool canMove = true;
 
     void Update()
     {
-        float horizontalDirection = Input.GetAxisRaw("Horizontal") * speed;
-        float verticalDirection = Input.GetAxisRaw("Vertical") * speed;
+        if (canMove)
+        {
+            float horizontalDirection = Input.GetAxisRaw("Horizontal") * speed;
+            float verticalDirection = Input.GetAxisRaw("Vertical") * speed;
 
-        rb.velocity = new Vector2 (horizontalDirection, verticalDirection);
+            if (new Vector2(horizontalDirection, verticalDirection).Equals(new Vector2(0, 0))) moving = false;
+            else moving = true;
+
+            anim.SetBool("moving", moving);
+            if (horizontalDirection != 0)
+                transform.localScale = new Vector3(horizontalDirection / Mathf.Abs(horizontalDirection), transform.localScale.y, transform.localScale.z);
+
+            rb.velocity = new Vector2(horizontalDirection, verticalDirection);
+        }
     }
+
+    public bool CanMove
+    {
+        set
+        {
+            canMove = value;
+        }
+    }
+    
 }
